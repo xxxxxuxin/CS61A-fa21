9cc58249711eac701657ecb1e8f8cbce123acf8a
psi <x.xu@hotmail.com>
2022-02-11 15:30:51 +0800

    Scheme

--- lab10/lab10.scm
+++ lab10/lab10.scm
@@ -1,9 +1,16 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) (cond
+                                          ((< num1 num2) -1)
+                                          ((= num1 num2) 0)
+                                          (else 1)
+                                          ))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) (lambda (inc) (+ num inc)))
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) (lambda (x) (f (g x))))
 
-(define lst 'YOUR-CODE-HERE)
+(define lst (cons '(1) 
+						(cons 2 
+							(cons (list 3 4) '(5))
+							)))
 
-(define (remove item lst) 'YOUR-CODE-HERE)
+(define (remove item lst) (filter (lambda (x) (not (= x item)))  lst))
