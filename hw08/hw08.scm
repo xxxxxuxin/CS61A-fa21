928a6446daedeee75920e6ab441fa2cb9dc641de
psi <x.xu@hotmail.com>
2022-02-11 17:03:39 +0800

    Scheme Lists

---
+++ hw08/hw08.scm
@@ -0,0 +1,27 @@
+(define (my-filter func lst) (cond
+		((null? lst) lst)
+		((func (car lst)) (cons (car lst) (my-filter func (cdr lst))))
+		(else (my-filter func (cdr lst)))
+		))
+
+(define (interleave s1 s2) (cond
+	((null?  s1 ) s2)
+	((null?  s2 ) s1)
+	(else (cons (car s1) (cons (car s2) (interleave (cdr s1) (cdr s2)))))
+
+	))
+
+(define (accumulate merger start n term)
+  (cond
+  	((= n 1) (merger start (term n)))
+  	(else (merger (accumulate merger start (- n 1) term) (term n)))
+
+  	))
+
+(define (no-repeats lst) (cond
+	((null? lst) lst)
+	(else (cons (car lst) (no-repeats 
+							(my-filter  (lambda (x) (not(= x (car lst))))  (cdr lst) )
+		)))
+
+	))
